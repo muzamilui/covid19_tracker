ee2c37644ab0cce9305718e0b026a2d851c0da0c
muzamilui <muzamillkhan36@gmail.com>
2022-02-06 06:54:57 -0800

    clone test

--- InfoBox.js
+++ InfoBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card,CardContent,Typography} from '@material-ui/core'
 import './InfoBox.css'
-
+// Added comments
 function InfoBox({title,cases,active,isRed,total ,...props}) {
     return (
         
